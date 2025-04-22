5b409e2763cad70517043c328dd356983e264bdb
kong-9527 <lesterzy@163.com>
2025-04-22 16:44:24 +0800

    photo-1

--- client/app/dashboard/photo-generator/page.tsx
+++ client/app/dashboard/photo-generator/page.tsx
@@ -1444,14 +1444,14 @@ export default function PhotoGenerator() {
               <h2 className="text-lg font-medium">Images</h2>
               
               <div className="flex space-x-2">
-                <button className="px-3 py-1 text-sm bg-emerald-600 text-white rounded-lg flex items-center">
+                {/* <button className="px-3 py-1 text-sm bg-emerald-600 text-white rounded-lg flex items-center">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 mr-1">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
                   </svg>
                   Download All
-                </button>
+                </button> */}
                 
-                <button className="p-1 text-emerald-600 bg-emerald-100 rounded-lg flex items-center justify-center">
+                {/* <button className="p-1 text-emerald-600 bg-emerald-100 rounded-lg flex items-center justify-center">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                   </svg>
@@ -1461,7 +1461,7 @@ export default function PhotoGenerator() {
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                   </svg>
-                </button>
+                </button> */}
               </div>
             </div>
             
